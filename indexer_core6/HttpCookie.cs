297089caf63480b5ec71c1f4365837c07746c650
Mohammed Elmoghazi <mohammed.elmoghazi@nagwa.com>
2022-07-16 23:11:21 +0200

    Add project files.

---
+++ indexer_core6/HttpCookie.cs
@@ -0,0 +1,21 @@
+﻿public class HttpCookie
+{
+    private readonly Dictionary<string, string> _dictionaray;
+    public DateTime Expiry { get; set; }
+    public HttpCookie()
+    {
+        _dictionaray = new Dictionary<string, string>();
+    }
+
+    public string this[string key]
+    {
+        get
+        {
+            return _dictionaray[key];
+        }
+        set
+        {
+            _dictionaray[key] = value;
+        }
+    }
+}
---
+++ indexer_core6/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Indexer is a way to access elements in a class that represent a list of values.
+Console.WriteLine("Indexer is a way to access elements in a class that represent a list of values.");
+
+HttpCookie cookie = new HttpCookie();
+cookie["name"] = "Mohammed";
+Console.WriteLine(cookie["name"]);
+
+Console.WriteLine("End!");
